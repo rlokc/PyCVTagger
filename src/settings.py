da731039c11b98275ef7743ebc7199be7c8ff6f0
Kirill Bolashev <kirik1007@gmail.com>
2017-05-17 12:21:34 +0300

    Update settings.py

--- src/settings.py
+++ src/settings.py
@@ -1,5 +1,5 @@
 class Settings():
     def __init__(self):
-        self.CLARIFAI_ID = "XzMuAKex2xXRjiWu43RjHgIos8tTRug92HCCdxJB"
-        self.CLARIFAI_SECRET = "gjhq3LVSmUlm1n1ukSPL7-nhAL05JjsBgGG4mp8N"
+        self.CLARIFAI_ID = "YOUT_CLARIFAI_ID"
+        self.CLARIFAI_SECRET = "YOUR_CLARIFAI_SECRET"
         self.GOOGLE_KEY = "YOUR_GOOGLE_CLOUD_VISION_KEY"
