67d22d7398546777327389213d62cb3631120797
rlokc <kirik1007@gmail.com>
2017-05-17 11:40:11 +0300

    settings moved to a python file for security reasons

---
+++ src/settings.py
@@ -0,0 +1,6 @@
+class Settings():
+    def __init__(self):
+        self.CLARIFAI_ID = "YOUR_CLARIFAI_ID"
+        self.CLARIFAI_SECRET = "YOUR_CLARIFAI_SECRET"
+        self.GOOGLE_ID = "YOUR_GOOGLE_CLOUD_VISION_ID"
+        self.GOOGLE_SECRET = "YOUR_GOOGLE_CLOUD_VISION_SECRET"
