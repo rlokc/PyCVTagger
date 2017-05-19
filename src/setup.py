e8ff0df3399e2faf57b5d89744accae4740bd819
Kirill Bolashev <kirik1007@gmail.com>
2017-05-19 13:51:18 +0300

    Update setup.py

--- src/setup.py
+++ src/setup.py
@@ -1,4 +1,14 @@
-from distutils.core import setup
-import py2exe
+#from distutils.core import setup
+#import py2exe
+#setup(console=['tagger.py'])
 
-setup(console=['tagger.py'])
+import sys
+from cx_Freeze import setup, Executable
+
+build_exe_option = {"packages": ["lxml"], "includes": ["lxml", "lxml.etree", "lxml._elementpath", "lxml.ElementInclude"]}
+
+setup( name = "CVTagger",
+       version = "0.1",
+       description = "Image tagger via Clarifai",
+       options = {"build_exe": build_exe_option},
+       executables = [Executable("tagger.py")])
